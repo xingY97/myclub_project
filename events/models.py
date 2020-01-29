8b2ea77425b8f5f6543c8c8ee16f6c24610b3e36
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-01-29 15:45:34 -0800

    more models

---
+++ events/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    name = models.CharField('event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    venue = models.CharField(max_length=120)
+    manager = models.CharField(max_length = 60)
+    description = models.TextField(blank=True)
